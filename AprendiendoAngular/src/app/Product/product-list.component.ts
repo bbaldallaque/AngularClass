25deee0b77709061e7f3d7c5cb0a58967ec07045
Bryant Stewart Baldallaque <bbaldallaque@mardom.com>
2019-12-09 14:04:03 -0400

    Property Binding

--- AprendiendoAngular/src/app/Product/product-list.component.ts
+++ AprendiendoAngular/src/app/Product/product-list.component.ts
@@ -6,6 +6,31 @@ import { Component } from '@angular/core';
 
 })
 export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  products: any[] = [
+  {
+    "productId": 2,
+    "productName": "Garden Cart",
+    "ProductCode": "GDN-0023",
+    "releaseDate": "March 18, 2019",
+    "descripcion": "15 gallon capacity rollin garden",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "assets/images/garden_cart.png"
+  },
 
+  {
+    "productId": 5,
+    "productName": "Hammer",
+    "ProductCode": "TXB-0048",
+    "releaseDate": "May 21, 2019",
+    "descripcion": "Curve claw stell hammer",
+    "price": 8.9,
+    "starRating": 4.8,
+    "imageUrl": "assets/images/hammer.png"
+  }
 
+];
 }
