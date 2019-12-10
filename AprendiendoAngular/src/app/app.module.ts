cbb04157f95b2c70b717d35e2ad2bd33d5ef8d89
Bryant Stewart Baldallaque <bbaldallaque@mardom.com>
2019-12-10 12:04:01 -0400

    More on Components

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ProductListComponent } from './Product/product-list.component';
 import { from } from 'rxjs';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
