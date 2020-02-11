eba32ef282006b3b60437821978d08527ca62245
bbaldallaquie <dr_baldallaque@hotmail.com>
2020-02-11 12:03:26 -0400

    Conectado con Asp.net

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -2,34 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './Home/welcome.component';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BlogPostsComponent } from './blog-posts/blog-posts.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogPostAddEditComponent } from './blog-post-add-edit/blog-post-add-edit.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    BlogPostsComponent,
+    BlogPostComponent,
+    BlogPostAddEditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', loadChildren: './Product/product.module#ProductModule' },
+    ReactiveFormsModule,
 
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    RouterModule.forRoot([
+
+      { path: '', component: BlogPostsComponent, pathMatch: 'full' },
+  { path: 'blogpost/:id', component: BlogPostComponent },
+  { path: 'add', component: BlogPostAddEditComponent },
+  { path: 'blogpost/edit/:id', component: BlogPostAddEditComponent },
+  { path: '**', redirectTo: '/' }
+      // { path: '', loadChildren: './Product/product.module#ProductModule' },
+      // { path: 'blogpost/:id', component: BlogPostComponent },
+      // { path: 'add', component: BlogPostAddEditComponent },
+      //  { path: 'blogpost/edit/:id', component: BlogPostAddEditComponent },
+      // { path: 'welcome', component: WelcomeComponent },
+      // { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      // { path: '', component: BlogPostsComponent, pathMatch: 'full' },
+      // { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+      // { path: '**', redirectTo: '/' }
     ])
   ],
 
 
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
